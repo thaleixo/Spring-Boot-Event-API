ee0e785ca83f2211b0c3cedddf7ea8ff2ba70d66
thaleixo <thiagohenriquealeixo1@gmail.com>
2025-02-18 15:46:00 -0300

    feat: add EventService functions

--- events/src/main/java/br/com/nlw/events/services/EventService.java
+++ events/src/main/java/br/com/nlw/events/services/EventService.java
@@ -1,5 +1,28 @@
 package br.com.nlw.events.services;
 
+import br.com.nlw.events.entitys.Event;
+import br.com.nlw.events.repositorys.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class EventService {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    public Event addNewEvent(Event event) {
+        event.setPrettyName(event.getTitle().toLowerCase().replaceAll(" ", "_"));
+        return eventRepository.save(event);
+    }
+
+    public List<Event> getAllEvents() {
+        return eventRepository.findAll();
+    }
+
+    public Event getEventByPrettyName(String prettyName) {
+        return eventRepository.getEventByPrettyName(prettyName);
+    }
 }
