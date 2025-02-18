a6dc56442d69272cdd0fa89c06f97d0503937049
thaleixo <thiagohenriquealeixo1@gmail.com>
2025-02-18 15:44:53 -0300

    feat: add EventRepository

--- events/src/main/java/br/com/nlw/events/repositorys/EventRepository.java
+++ events/src/main/java/br/com/nlw/events/repositorys/EventRepository.java
@@ -4,4 +4,5 @@ import br.com.nlw.events.entitys.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventRepository extends JpaRepository<Event, Integer> {
+    Event getEventByPrettyName(String prettyName);
 }
