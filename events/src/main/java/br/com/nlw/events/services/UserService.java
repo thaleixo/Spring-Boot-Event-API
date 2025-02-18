93a2e11f3751db8ad372282f09aa1b364e24d786
thaleixo <thiagohenriquealeixo1@gmail.com>
2025-02-18 16:07:35 -0300

    feat: add UserService

---
+++ events/src/main/java/br/com/nlw/events/services/UserService.java
@@ -0,0 +1,12 @@
+package br.com.nlw.events.services;
+
+import br.com.nlw.events.repositorys.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+}
